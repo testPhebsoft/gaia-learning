4ee4cd2c4c20554b05cb76f74dc909eeebb32101
test.phebsoft <ismailahmad717@gmail.com>
2024-06-24 16:03:25 +0500

    calendar

--- src/components/Calender.jsx
+++ src/components/Calender.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import format from 'date-fns/format'
+import parse from 'date-fns/parse'
+import startOfWeek from 'date-fns/startOfWeek'
+import getDay from 'date-fns/getDay'
+import enUS from 'date-fns/locale/en-US'
 
+const locales = {
+  'en-US': enUS,
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
 const Calender = () => {
-  return <div>Calendar lol</div>;
+  return (
+    <Calendar
+      localizer={localizer}
+      events={myEventsList}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 500 }}
+    />
+  )
 };
 
 export default Calender;
