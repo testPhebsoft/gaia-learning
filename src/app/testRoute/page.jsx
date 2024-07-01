b7399ed635ce824715a913d1d912cc26e23ccb55
chadium1st <ismailahmad717@gmail.com>
2024-07-01 13:11:34 +0500

    test route commented out

--- src/app/testRoute/page.jsx
+++ src/app/testRoute/page.jsx
@@ -1,28 +1,27 @@
-"use client"
-import { useEffect, useState } from "react";
-import { Login } from "../../actions/Auth"
+// "use client"
+// import { useEffect, useState } from "react";
+// import { Login } from "../../actions/Auth"
 
-export default function Page() {
-    const [data,setData] = useState(null)
+// export default function Page() {
+//     const [data,setData] = useState(null)
     
-// useEffect(()=>{
-//     const run = async () => {
-//         let data =  await Login(username , password);
-//        setData(data)      
+// // useEffect(()=>{
+// //     const run = async () => {
+// //         let data =  await Login(username , password);
+// //        setData(data)      
 
-//     }
+// //     }
     
-//     run();
-// },[])
+// //     run();
+// // },[])
 
 
-const handleLogin =  async(username , password) => { 
-    let data =  await Login(username , password);
-    setData(data)   
-}
-    
-    return <> 
-    {data &&  Object.keys(data).map((key)=> <h1>{data[key]}</h1>)}
-    <button onClick={()=>handleLogin("n0b","qwety")} > Login</button>
-        </>
-}
+// const handleLogin =  async(username , password) => { 
+//     let data =  await Login(username , password);
+//     setData(data)   
+// }
+//     return <> 
+//     {data &&  Object.keys(data).map((key)=> <h1>{data[key]}</h1>)}
+//     <button onClick={()=>handleLogin("n0b","qwety")} > Login</button>
+//         </>
+// }
